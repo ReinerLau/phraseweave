719152af63c5b7e8e2c9b51ec249e7c2008330ec
ReinerLau <lk850593913@gmail.com>
2026-09-20 20:35:59 +0800

    fix(client): unregister service workers in development

--- apps/client/plugins/register-sw.client.ts
+++ apps/client/plugins/register-sw.client.ts
@@ -1,7 +1,16 @@
 import { defineNuxtPlugin } from "nuxt/app";
 
 export default defineNuxtPlugin(() => {
-  if ("serviceWorker" in navigator) {
-    void navigator.serviceWorker.register("./sw.js", { scope: "./" });
+  if (!("serviceWorker" in navigator)) return;
+
+  if (import.meta.dev) {
+    void navigator.serviceWorker
+      .getRegistrations()
+      .then((registrations) =>
+        Promise.all(registrations.map((registration) => registration.unregister())),
+      );
+    return;
   }
+
+  void navigator.serviceWorker.register("/sw.js", { scope: "/" });
 });
