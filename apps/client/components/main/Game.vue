39ea152c47bdfc0bf777be077a0a62351dd77d2f
ReinerLau <lk850593913@gmail.com>
2026-09-22 15:05:58 +0800

    feat(client): expand practice input focus area

--- apps/client/components/main/Game.vue
+++ apps/client/components/main/Game.vue
@@ -1,10 +1,16 @@
 <template>
-  <template v-if="currentGameMode === GameMode.Dictation">
-    <ModeDictationMode />
-  </template>
-  <template v-else-if="currentGameMode === GameMode.ChineseToEnglish">
-    <ModeChineseToEnglishMode />
-  </template>
+  <div
+    class="flex h-full min-h-0 w-full min-w-0 flex-1 items-center justify-center"
+    data-testid="practice-focus-area"
+    @click="handleFocusAreaClick"
+  >
+    <template v-if="currentGameMode === GameMode.Dictation">
+      <ModeDictationMode />
+    </template>
+    <template v-else-if="currentGameMode === GameMode.ChineseToEnglish">
+      <ModeChineseToEnglishMode />
+    </template>
+  </div>
 
   <MainTips />
   <MainSummary />
@@ -15,10 +21,20 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
 
+import { useQuestionInput } from "~/components/main/QuestionInput/questionInputHelper";
 import { courseTimer } from "~/composables/courses/courseTimer";
+import { useGameMode as useQuestionGameMode } from "~/composables/main/game";
 import { GameMode, useGameMode } from "~/composables/user/gameMode";
 
 const { currentGameMode } = useGameMode();
+const { isQuestion } = useQuestionGameMode();
+const { focusInput } = useQuestionInput();
+
+function handleFocusAreaClick() {
+  if (isQuestion()) {
+    focusInput();
+  }
+}
 
 onMounted(() => {
   courseTimer.reset();
